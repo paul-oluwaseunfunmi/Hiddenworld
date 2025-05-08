6f58cc595d246e68bb359c49876039391f37e4c9
Paul <pauloluwaseunfunmi19@gmail.com>
2025-05-08 13:00:01 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const signupbutton = document.getElementById('signupbutton')
+const signinbutton = document.getElementById('signinbutton')
+const signinform = document.getElementById('signin')
+const signupform = document.getElementById('signup')
+
+signupbutton.addEventListener('click',function(){
+    signinform.style.display='none';
+    signupform.style.display='block';
+})
+signinbutton.addEventListener('click', function(){
+    signinform.style.display='block';
+    signupform.style.display='none';
+})
